e0d08c8d8616c011b9dc2bc9b54536a3e7f23b9d
duchaokha <duchaokha@gmail.com>
2022-10-19 21:22:46 +0000

    Update apply file and deployment

---
+++ apply-deployment.sh
@@ -0,0 +1,5 @@
+kubectl apply -f voting-deployment.yaml
+kubectl apply -f redis-deployment.yaml
+kubectl apply -f worker-deployment.yaml
+kubectl apply -f postgres-deployment.yaml
+kubectl apply -f result-deployment.yaml
---
+++ apply-service.sh
@@ -0,0 +1,4 @@
+kubectl apply -f voting-service.yaml
+kubectl apply -f redis-service.yaml
+kubectl apply -f postgres-service.yaml
+kubectl apply -f result-service.yaml
